f2195654c6deb07a49ac6026ab6e17809b57fa71
kinass <345865dc@gmail.com>
2024-12-03 14:23:09 +0800

    Site updated: 2024-12-03 14:23:08

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/03/hello-world/"];function toRandomPost(){
+var posts=["2024/12/03/hello-world/","2024/12/03/test/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
