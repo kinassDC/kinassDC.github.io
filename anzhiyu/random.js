a4e88dc6492edf3ef5734bb3c3de8f8e09309044
kinass <345865dc@gmail.com>
2024-12-08 16:45:17 +0800

    Site updated: 2024-12-08 16:45:16

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/05/散列查找/"];function toRandomPost(){
+var posts=["2024/12/08/排序算法/","2024/12/05/散列查找/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
