936312af7bbafc19930ceb523a9424928c1b3c8e
kinass <345865dc@gmail.com>
2024-12-18 19:10:12 +0800

    Site updated: 2024-12-18 19:10:11

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/10/vscode配置/","2024/12/10/docker使用/","2024/12/05/散列查找/","2024/12/08/排序算法/"];function toRandomPost(){
+var posts=["2024/12/10/docker使用/","2024/12/10/vscode配置/","2024/12/08/排序算法/","2024/12/05/散列查找/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
