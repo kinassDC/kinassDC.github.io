bd0c201a55950ca36e316b735adc563db82e08e1
kinass <345865dc@gmail.com>
2025-01-08 12:25:54 +0800

    Site updated: 2025-01-08 12:25:53

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/10/docker使用/","2024/12/18/git的使用/","2024/12/08/排序算法/","2024/12/10/vscode配置/","2024/12/05/散列查找/"];function toRandomPost(){
+var posts=["2024/12/10/docker使用/","2024/12/18/git的使用/","2024/12/27/vim使用技巧-md/","2024/12/10/vscode配置/","2024/12/08/排序算法/","2024/12/27/vim使用技巧/","2024/12/05/散列查找/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
